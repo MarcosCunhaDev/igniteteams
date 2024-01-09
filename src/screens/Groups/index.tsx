614a6d008a1c5494044aba57d2e44ac1fcd0b4b7
MarcosCunhaDev <marcoswrc2@gmail.com>
2024-01-09 11:20:19 -0300

    Config: Styled Components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,21 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text } from "react-native";
+import * as S from "./styles";
 
 export function Groups() {
   return (
-    <View style={styles.container}>
+    <S.Container>
       <Text>Groups</Text>
       <Text>React native test</Text>
       <Text>testando Hot reload</Text>
-    </View>
+    </S.Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
