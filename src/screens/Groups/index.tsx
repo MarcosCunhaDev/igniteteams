6974e81ab82bf1aba5d5207aa57cf0508825320c
MarcosCunhaDev <marcoswrc2@gmail.com>
2024-01-11 15:24:39 -0300

    Add Flatlist to Groups screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,17 +2,24 @@ import { Header } from "@components/Header";
 import * as S from "./styles";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Groups() {
+  const [groups, setGroups] = useState([
+    "Galera da Gringo",
+    "Amigos",
+    "Nintendistas",
+  ]);
+
   return (
     <S.Container>
       <Header version="primary" />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard
-        title="Galera do Ignite"
-        onPress={() => {
-          console.log("Clicou aqui");
-        }}
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item, index }) => <GroupCard title={item} />}
       />
     </S.Container>
   );
