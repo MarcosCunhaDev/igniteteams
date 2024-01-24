8824f79fe3c8271a96da627a495bd7f953d55202
MarcosCunhaDev <marcoswrc2@gmail.com>
2024-01-24 12:05:57 -0300

    Fix: glitch when navigation between screens

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { AppRoutes } from "./app.routes";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { View } from "react-native";
+import { useTheme } from "styled-components/native";
 
 export const Routes = () => {
+  const theme = useTheme();
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <AppRoutes />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <View style={{ flex: 1, backgroundColor: theme.colors.gray_600 }}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <AppRoutes />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </View>
   );
 };
